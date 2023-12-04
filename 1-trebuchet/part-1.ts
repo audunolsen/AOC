31ebdb835784c721328e62ff26f376bbc1f0f61a
Audun Meek Olsen <audunmeekolsen@gmail.com>
2023-12-04 11:54:51 +0100

    WIP

--- 1-trebuchet/part-1.ts
+++ 1-trebuchet/part-1.ts
@@ -1,6 +1,6 @@
 // deno run --allow-read 1-trebuchet/part-1.ts
 
-const input = await Deno.readTextFile("1/input.txt")
+const input = await Deno.readTextFile("1-trebuchet/puzzle-input.txt")
 
 const sum = input.split("\n").reduce((acc, line) => {
     const chars = line.split("")
