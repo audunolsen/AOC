b51329473ac03698031a7c0e844f5e9ec1a36f02
Audun Meek Olsen <audunmeekolsen@gmail.com>
2023-12-04 13:38:41 +0100

    WIP

--- 2-cube-conundrum/part-1.ts
+++ 2-cube-conundrum/part-1.ts
@@ -1,33 +1,6 @@
 // deno run --allow-read 2-cube-conundrum/part-1.ts
 
-const input = await Deno.readTextFile("2-cube-conundrum/puzzle-input.txt")
-
-type GameRecord = Array<{
-    gameId: number
-    pulls: Array<Record<string, number>>
-}>
-
-const gameRecord: GameRecord = input.split("\n").map((line, i) => {
-    line = line.replace(/\s/g, "")
-    const [, rawGameData] = line.split(":")
-
-    const gameData = rawGameData.split(";").map(pull => {
-        const matches = [...pull.matchAll(/(?<qty>\d+)(?<color>\w+)/g)]
-
-        return matches.reduce<Record<string, number>>((acc, match) => {
-            const color = match?.groups?.color
-
-            return !color
-                ? acc
-                : { ...acc, [color]: parseInt(match?.groups?.qty ?? "0") }
-        }, {})
-    })
-
-    return {
-        gameId: i + 1,
-        pulls: gameData,
-    }
-}, {})
+import { gameRecord } from "./index.ts"
 
 const gameCriteria = {
     red: 12,
--- 2-cube-conundrum/part-2.ts
+++ 2-cube-conundrum/part-2.ts
@@ -1,1 +1,20 @@
 // deno run --allow-read 2-cube-conundrum/part-2.ts
+
+import { gameRecord } from "./index.ts"
+
+const bagsPerGame = gameRecord.map(game => {
+    return game.pulls.reduce<Record<string, number>>((acc, pull) => {
+        for (const [color, qty] of Object.entries(pull)) {
+            acc[color] ??= qty
+            if (qty > acc[color]) acc[color] = qty
+        }
+
+        return acc
+    }, {})
+})
+
+const ultimatePower = bagsPerGame.reduce((acc, bag) => {
+    return acc + Object.values(bag).reduce((a, b) => a * b)
+}, 0)
+
+console.log(ultimatePower)
